0c53b812bc1ed676ea2ef0817918c4a12d526737
Roman Kr <r@politex.org.ua>
2019-01-24 16:21:08 +0200

    Tab/Spaces fix

--- examples/simpleFlashNVS.ino
+++ examples/simpleFlashNVS.ino
@@ -6,14 +6,16 @@
  * It wraps main NVS functionality into the Arduino-styled C++ class. 
  */
 
+bool res;
+
 void setup() {   
     NVS.begin();
 }
 
-void loop() {
-	bool res;
-	/*** Int ***/
-	// write to flash
+
+void loop() {	
+    /*** Int ***/
+    // write to flash
     const uint64_t ui64_set = 0x1122334455667788;
     res = NVS.setInt("longInt", ui64_set);
     // read from flash
@@ -27,15 +29,14 @@ void loop() {
     // read from flash
     String st = NVS.getString("st");
 
-
-	/*** BLOB ***/
-	//write to flash
-	uint8_t blolb_set[] = {1,2,3,99,100, 0xEE, 0xFE, 0xEE};
+    /*** BLOB ***/
+    //write to flash
+    uint8_t blolb_set[] = {1,2,3,99,100, 0xEE, 0xFE, 0xEE};
     res = NVS.setBlob("blob", blolb_set, sizeof(blolb_set));
-	// read from flash
+    // read from flash
     size_t blobLength = NVS.getBlobSize("blob");    
     uint8_t blob[blobLength];
     res = NVS.getBlob("blob", blob, sizeof(blob));
     
-	delay(1000);
+    delay(1000);
 }
